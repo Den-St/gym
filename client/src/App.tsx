66b536b05a9ca954d5d1453cc1e2d60dac4b0be0
denisstuenko <stuenkden@gmail.com>
2022-08-20 14:44:39 +0300

    project done client frontend client data from database admin panel admin authorization review create form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import '../src/components/theme/fonts.css'
+import {Layout} from "./containers/Layout";
+import {Main} from "./containers/Main";
+import {Normalize} from "styled-normalize";
+import {Route, Routes} from "react-router-dom";
+import {Admin} from "./admin/containers";
+import {LoginPage} from "./admin/containers/loginPage";
 function App() {
-  return <></>
+  return <><Layout>
+    <Routes>
+      <Route path={'/'} element={<Main/>}/>
+      <Route path={"/admin"} element={<Admin/>}/>
+      <Route path={"/admin/login"} element={<LoginPage/>}/>
+    </Routes>
+  </Layout>
+  <Normalize/></>
 }
 
 export default App;
