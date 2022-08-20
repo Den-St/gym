9547639ce93ebe0b479d8310bea6a80ad5ac7eff
denisstuenko <stuenkden@gmail.com>
2022-08-20 14:44:39 +0300

    project done client frontend client data from database admin panel admin authorization review create form

---
+++ client/src/components/theme/palette.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    orangePrimary: "#FFA400",
+    beige:"#FFF4DA",
+    beigeLight:"#FFF6E5",
+    lightGray:"#F0F0F0",
+    darkBlue:"#111C4E",
+    extraLightGray:"#EDEDED",
+    borderGray:"#C8C8C8",
+    dateGray:"#9A9AB0",
+    commentColor:"#515151",
+    orangeBorder:"#FFCB6D",
+    textGray:"#868686",
+}
